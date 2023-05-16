1128d5aa993ae467e0e9806fc4a8d322d3f44315
Denis Mosin <denmsin@mail.ru>
2023-05-16 23:25:04 +0500

    Initial commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,85 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected OptionalInt age;
+    protected String adress;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = OptionalInt.of(age);
+        ;
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public boolean hasAge() {
+        return age.isPresent();
+    }
+
+    public boolean hasAddress() {
+        if (adress == null) return false;
+        return true;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getSurname() {
+
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+
+        return age;
+    }
+
+    public String getAddress() {
+
+        return adress;
+    }
+
+    public void setAddress(String address) {
+
+        this.adress = address;
+    }
+
+    public void happyBirthday() {
+        this.age = OptionalInt.of(age.getAsInt() + 1);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, adress);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAddress(this.adress);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", adress='" + adress + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
---
+++ src/main/java/PersonBuilder.java
@@ -0,0 +1,40 @@
+public class PersonBuilder{
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+
+    public Person build() {
+        Person person;
+        if (surname == null || name == null)
+            throw new IllegalStateException("введите значение");
+        if (age > 0) {
+            person = new Person(name, surname, age);
+        } else {
+            person = new Person(name, surname);
+        }
+        person.setAddress(address);
+        return person;
+    }
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) throw new IllegalArgumentException("неверный возраст");
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+}
